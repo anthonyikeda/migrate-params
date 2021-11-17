ad07d574a35689a440504b29d70b055d681b6573
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-11-17 10:10:40 -0800

    Fixed sql syntax for adding the primary key constrinat and generating the ids

--- tables.sql
+++ tables.sql
@@ -1,10 +1,10 @@
 create table ${company_name} (
-   id bigint not null,
+   id bigint not null generated always as identity,
    employee_name varchar(40),
    employee_role varchar(40)
 );
 
-create constraint pk_company_id primary key on ${company_name}(id);
+alter table ${company_name} add constraint pk_company_id primary key (id);
 
 insert into ${company_name} (employee_name, employee_role) values ('Ben', 'developer');
 insert into ${company_name} (employee_name, employee_role) values ('Sarah', 'developer');
